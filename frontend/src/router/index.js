31df01ba07854a3b6142b1d650027f86fe424498
Taehoon <lth9029@gmail.com>
2024-01-10 21:54:41 +0900

    Complete test for SPA implementation

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import PingComponent from '../components/PingComponent.vue'
+import PingComponent from '../components/layout/PingComponent.vue'
 
 const routes = [
   {
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <HeaderComponent/>
     <img alt="Vue logo" src="../assets/logo.png">
+    <PingComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderComponent from '../components/layout/HeaderComponent.vue'
+import PingComponent from '../components/layout/PingComponent.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HeaderComponent,
+    PingComponent,
   }
 }
 </script>
